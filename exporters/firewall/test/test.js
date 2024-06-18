b4b948f3adb5439f5cffda7a0c4c767059590081
chaoli <lichao01.ah@chinatelecom.cn>
2024-06-18 16:32:27 +0800

    增加模型说明

--- exporters/firewall/test/test.js
+++ exporters/firewall/test/test.js
@@ -4,7 +4,7 @@ const h3Parser = require('../manufacturer/firewall-h3.js');
 
 async function readHuaweiConf() {
     try {
-      var data = await fs.promises.readFile('node_exporter/exporters/firewall/test/huawei-configuration.txt', 'utf8');
+      var data = await fs.promises.readFile('node_exporter/exporters/firewall/test/firewall-huawei-configuration.txt', 'utf8');
       var tableData=huaweiParser.getConfInfo(data);
       console.log(tableData)
     } catch (err) {
@@ -14,7 +14,7 @@ async function readHuaweiConf() {
 
   async function readH3Conf() {
     try {
-      var data = await fs.promises.readFile('node_exporter/exporters/firewall/test/h3-configuration.txt', 'utf8');
+      var data = await fs.promises.readFile('node_exporter/exporters/firewall/test/firewall-h3-configuration.txt', 'utf8');
       var tableData=h3Parser.getConfInfo(data);
       console.log(tableData)
     } catch (err) {
